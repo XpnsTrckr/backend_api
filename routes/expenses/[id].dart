78b4c4ed1732df0be3eae363ba8093394bfb1010
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2022-11-21 15:46:24 -0300

    feat: Added expenses crud endpoints.

---
+++ routes/expenses/[id].dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:xpns_trckr_api/data_source/expenses_data_source.dart';
+import 'package:xpns_trckr_api/models/expense.dart';
+
+FutureOr<Response> onRequest(RequestContext context, int id) async {
+  final dataSource = context.read<ExpensesDataSource>();
+  final expense = await dataSource.read(id);
+
+  if (expense == null) {
+    return Response(statusCode: HttpStatus.notFound, body: 'Not found');
+  }
+
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return Response.json(body: expense);
+    case HttpMethod.put:
+      return _put(context, id);
+    case HttpMethod.delete:
+      return _delete(context, id);
+    case HttpMethod.head:
+    case HttpMethod.options:
+    case HttpMethod.patch:
+    case HttpMethod.post:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
+
+Future<Response> _put(RequestContext context, int id) async {
+  final dataSource = context.read<ExpensesDataSource>();
+  final json = context.request.json() as Map<String, dynamic>;
+  final expense = Expense.fromJson(json);
+
+  await dataSource.update(id, expense);
+
+  return Response.json(body: expense);
+}
+
+Future<Response> _delete(RequestContext context, int id) async {
+  final dataSource = context.read<ExpensesDataSource>();
+  await dataSource.delete(id);
+
+  return Response(statusCode: HttpStatus.noContent);
+}
---
+++ routes/expenses/index.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:xpns_trckr_api/data_source/expenses_data_source.dart';
+import 'package:xpns_trckr_api/models/expense.dart';
+
+FutureOr<Response> onRequest(RequestContext context) async {
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return _get(context);
+    case HttpMethod.post:
+      return _post(context);
+    case HttpMethod.delete:
+    case HttpMethod.head:
+    case HttpMethod.options:
+    case HttpMethod.patch:
+    case HttpMethod.put:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
+
+Future<Response> _get(RequestContext context) async {
+  final dataSource = context.read<ExpensesDataSource>();
+  final expenses = await dataSource.readAll();
+  return Response.json(body: expenses);
+}
+
+Future<Response> _post(RequestContext context) async {
+  final dataSource = context.read<ExpensesDataSource>();
+  final json = context.request.json() as Map<String, dynamic>;
+  final expense = Expense.fromJson(json);
+
+  await dataSource.create(expense);
+
+  return Response.json(statusCode: HttpStatus.created);
+}
