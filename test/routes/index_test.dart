897ddc973c536988ed8e52b40804434507a0c9e8
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2022-11-21 16:18:00 -0300

    chore: fixed index_test

--- test/routes/index_test.dart
+++ test/routes/index_test.dart
@@ -10,13 +10,18 @@ class _MockRequestContext extends Mock implements RequestContext {}
 
 void main() {
   group('GET /', () {
-    test('responds with a 200 and "Welcome to Xpns Trckr API!".', () {
+    test('responds with a 200 and welcome message', () {
       final context = _MockRequestContext();
       final response = route.onRequest(context);
+      const welcome = '''
+Welcome to Xpns Trckr API!
+Please choose a version to work with.
+''';
+
       expect(response.statusCode, equals(HttpStatus.ok));
       expect(
         response.body(),
-        completion(equals('Welcome to Xpns Trckr API!')),
+        completion(equals(welcome)),
       );
     });
   });
