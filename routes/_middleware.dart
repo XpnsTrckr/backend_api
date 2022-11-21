44cf3b61614fa9f5b62c92efd39a47be132400ae
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2022-11-21 15:53:02 -0300

    feat: Moved to v1 folder.

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,9 +1,4 @@
 import 'package:dart_frog/dart_frog.dart';
-import 'package:xpns_trckr_api/data_source/expenses_data_source.dart';
-import 'package:xpns_trckr_api/data_source/in_memory_expenses_data_source.dart';
 
-final _dataSource = InMemoryExpensesDataSource();
-
-Handler middleware(Handler handler) => handler
-    .use(requestLogger())
-    .use(provider<ExpensesDataSource>((_) => _dataSource));
+/// Middleware used for all versions.
+Handler middleware(Handler handler) => handler.use(requestLogger());
