8c056cc8efca052d7a7d221038d8de16e1e8f156
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2022-11-22 17:32:39 -0300

    test: added ExpensesDataSource unit tests.

---
+++ test/lib/data_source/expenses_data_source_test.dart
@@ -0,0 +1,26 @@
+import 'package:test/test.dart';
+import 'package:xpns_trckr_api/data_source/expenses_data_source.dart';
+import 'package:xpns_trckr_api/models/expense.dart';
+
+class _ExpensesDataSource implements ExpensesDataSource {
+  @override
+  Future<void> create(Expense expense) => throw UnimplementedError();
+
+  @override
+  Future<void> delete(int id) => throw UnimplementedError();
+
+  @override
+  Future<Expense?> read(int id) => throw UnimplementedError();
+
+  @override
+  Future<List<Expense>> readAll() => throw UnimplementedError();
+
+  @override
+  Future<void> update(int id, Expense expense) => throw UnimplementedError();
+}
+
+void main() {
+  group('ExpensesDataSource', () {
+    test('can be implemented', () => expect(_ExpensesDataSource(), isNotNull));
+  });
+}
