4c613b5bf5cd01cb590cdf814395e126664ef59d
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2022-11-22 11:47:42 -0300

    fix: Unique Id variable name.

--- routes/v1/expenses/[id].dart
+++ routes/v1/expenses/[id].dart
@@ -6,9 +6,9 @@ import 'package:xpns_trckr_api/data_source/expenses_data_source.dart';
 import 'package:xpns_trckr_api/models/expense.dart';
 
 FutureOr<Response> onRequest(RequestContext context, String query) async {
-  final uid = int.tryParse(query);
+  final id = int.tryParse(query);
 
-  if (uid == null) {
+  if (id == null) {
     return Response(
       statusCode: HttpStatus.badRequest,
       body: 'Query ID must be an integer',
@@ -16,7 +16,7 @@ FutureOr<Response> onRequest(RequestContext context, String query) async {
   }
 
   final dataSource = context.read<ExpensesDataSource>();
-  final expense = await dataSource.read(uid);
+  final expense = await dataSource.read(id);
 
   if (expense == null) {
     return Response(statusCode: HttpStatus.notFound, body: 'Not found');
@@ -26,9 +26,9 @@ FutureOr<Response> onRequest(RequestContext context, String query) async {
     case HttpMethod.get:
       return Response.json(body: expense);
     case HttpMethod.put:
-      return _put(context, uid);
+      return _put(context, id);
     case HttpMethod.delete:
-      return _delete(context, uid);
+      return _delete(context, id);
     case HttpMethod.head:
     case HttpMethod.options:
     case HttpMethod.patch:
@@ -37,19 +37,19 @@ FutureOr<Response> onRequest(RequestContext context, String query) async {
   }
 }
 
-Future<Response> _put(RequestContext context, int uid) async {
+Future<Response> _put(RequestContext context, int id) async {
   final dataSource = context.read<ExpensesDataSource>();
   final json = context.request.json() as Map<String, dynamic>;
   final expense = Expense.fromJson(json);
 
-  await dataSource.update(uid, expense);
+  await dataSource.update(id, expense);
 
   return Response.json(body: expense);
 }
 
-Future<Response> _delete(RequestContext context, int uid) async {
+Future<Response> _delete(RequestContext context, int id) async {
   final dataSource = context.read<ExpensesDataSource>();
-  await dataSource.delete(uid);
+  await dataSource.delete(id);
 
   return Response(statusCode: HttpStatus.noContent);
 }
